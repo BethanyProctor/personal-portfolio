871f159f76ab3d847b78de0af4ff88d0b963d85d
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-07-30 12:53:49 -0400

    working on animation for banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,46 @@
+import { useState, useEffect } from 'react';
+
 import { Col, Container, Row } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
 import ProfilePicture from '../assets/profile-picture.jpg'
 
 export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Full Stack Dev", "Web Designer", "Game Developer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            ticker();
+        }, delta)
+
+        return () => { clearInterval(ticker)}
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta /2)
+        }
+
+        if (isDeleting && updatedText === fullText) {
+            setIsDeleting(True);
+            setDelta(period)
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum +1);
+            setDelta(500)
+        }
+    }
+
     return (
         <section className='banner' id="home">
             <Container>
