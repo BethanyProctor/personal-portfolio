20dc04b59b20488ef8b416e3f6bbf79268840e06
Bethany Proctor <bethanyanneproctor@gmail.com>
2024-07-30 11:45:06 -0400

    setting up navbar component

--- Personal-Portfolio/src/components/NavBar.jsx
+++ Personal-Portfolio/src/components/NavBar.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
 import { NavBar, Container } from "react-bootstrap";
 
 export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect{() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        }
+
+        window.addEventListener("scroll", onScroll)
+
+        return () => window.removeEventListener("scroll", onScroll)
+    }, []}
     return (
-        <Navbar expand="lg">
+        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
           <Container>
             <Navbar.Brand href="#home">Bethany Proctor</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav">
@@ -10,11 +27,13 @@ export const NavBar = () => {
             </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#home" className={activeLink === "home" ? "active-navbar-link" : 'navbar-link'}>Home</Nav.Link>
                 <Nav.Link href="#about">About Me</Nav.Link>
                 <Nav.Link href="#projects">My Projects</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
               </Nav>
+              <span className="navbar-text">
+                <button className="vvd" onClick={()=>console.log('lets connect')}><span>Contact Me</span></button>
+              </span>
             </Navbar.Collapse>
           </Container>
         </Navbar>
